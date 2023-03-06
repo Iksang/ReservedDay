5332d149680d83aa4b02ad70cfa51eecd60dca6b
Iksang <thsdlrtkd@gmail.com>
2023-03-06 22:52:45 +0900

    1차 수정

--- app/src/main/java/com/ikstudio/reservedday/calendar/CalendarActivity.kt
+++ app/src/main/java/com/ikstudio/reservedday/calendar/CalendarActivity.kt
@@ -3,17 +3,14 @@ package com.ikstudio.reservedday.calendar
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import com.ikstudio.reservedday.R
 import com.ikstudio.reservedday.databinding.ActivityCalendarBinding
-import java.io.FileInputStream
-import java.io.FileOutputStream
+import java.util.*
 
 class CalendarActivity : AppCompatActivity() {
-    var userID: String = "userID"
-    lateinit var fname: String
-    lateinit var str: String
     lateinit var binding: ActivityCalendarBinding
     val calendarViewModel:CalendarViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,9 +21,74 @@ class CalendarActivity : AppCompatActivity() {
 
         binding.title.text = "달력 일기장"
 
+        setListener()
+        setObserve()
+    }
+    fun setListener(){
         binding.calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
-            calendarViewModel.dateChange(year, month, dayOfMonth)
+            binding.diaryTextView.setText(String.format("%d / %d / %d", year, month+1, dayOfMonth))
+            binding.contextEditText.setText("")
+            calendarViewModel.checkDay(year, month, dayOfMonth, filesDir)
         }
+    }
+
+    fun setObserve(){
+        calendarViewModel.statetype.observe(this){
+            when(it){
+                StateType.TodayInit -> setTodayInitState()
+                StateType.ShowSchedule -> showScheduleState()
+                StateType.Save -> setSaveBtnClick()
+                StateType.Update -> onUpdateBtnClick()
+                StateType.Delete -> onDeleteBtnClick()
+                else -> Unit
+            }
+        }
+    }
+
+    fun setTodayInitState(){
+        val today = Calendar.getInstance()
+        binding.diaryTextView.setText(String.format("%d / %d / %d", today.get(Calendar.YEAR),
+            today.get(Calendar.MONTH)+1, today.get(Calendar.DAY_OF_MONTH)))
+        binding.contextEditText.setText("")
+        calendarViewModel.checkDay( today.get(Calendar.YEAR),today.get(Calendar.MONTH), today.get(Calendar.DAY_OF_MONTH), filesDir)
+    }
+
+    fun showScheduleState(){
+        binding.contextEditText.visibility = View.INVISIBLE
+        binding.diaryContent.visibility = View.VISIBLE
+        binding.saveBtn.visibility = View.INVISIBLE
+        binding.updateBtn.visibility = View.VISIBLE
+        binding.deleteBtn.visibility = View.VISIBLE
+    }
+
+
+    fun setSaveBtnClick(){
+        calendarViewModel.saveDiary(binding.contextEditText.text.toString() , filesDir)
+        binding.contextEditText.visibility = View.INVISIBLE
+        binding.saveBtn.visibility = View.INVISIBLE
+        binding.updateBtn.visibility = View.VISIBLE
+        binding.deleteBtn.visibility = View.VISIBLE
+        calendarViewModel.contents.value = binding.contextEditText.text.toString()
+        binding.diaryContent.visibility = View.VISIBLE
+    }
+
+    fun onUpdateBtnClick(){
+        binding.contextEditText.visibility = View.VISIBLE
+        binding.diaryContent.visibility = View.INVISIBLE
+        binding.contextEditText.setText( calendarViewModel.contents.value)
+        binding.saveBtn.visibility = View.VISIBLE
+        binding.updateBtn.visibility = View.INVISIBLE
+        binding.deleteBtn.visibility = View.INVISIBLE
+    }
+
+    fun onDeleteBtnClick(){
+        binding.diaryContent.visibility = View.INVISIBLE
+        binding.updateBtn.visibility = View.INVISIBLE
+        binding.deleteBtn.visibility = View.INVISIBLE
+        binding.contextEditText.setText("")
+        binding.contextEditText.visibility = View.VISIBLE
+        binding.saveBtn.visibility = View.VISIBLE
+        calendarViewModel.removeDiary(filesDir)
     }
 
 }
--- app/src/main/java/com/ikstudio/reservedday/calendar/CalendarViewModel.kt
+++ app/src/main/java/com/ikstudio/reservedday/calendar/CalendarViewModel.kt
@@ -1,106 +1,51 @@
 package com.ikstudio.reservedday.calendar
 
 import android.annotation.SuppressLint
-import android.app.Application
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import java.io.FileInputStream
-import java.io.FileOutputStream
+import java.io.File
 
-class CalendarViewModel(application: Application) : AndroidViewModel(application) {
+enum class StateType{
+    TodayInit, ShowSchedule, Update, Save, Delete
+}
+
+class CalendarViewModel : ViewModel() {
     var userID: String = "userID"
     lateinit var fname: String
-    lateinit var str: String
-    var diaryTextView : MutableLiveData<String> = MutableLiveData("")
-    var contents : MutableLiveData<String> = MutableLiveData("")
-    var contextEditText : MutableLiveData<String> = MutableLiveData("")
-    var showDiaryTextView : MutableLiveData<Boolean> = MutableLiveData(false)
-    var showContextEditText : MutableLiveData<Boolean> = MutableLiveData(false)
-    var showDiaryContent : MutableLiveData<Boolean> = MutableLiveData(false)
-    var showSaveBtn : MutableLiveData<Boolean> = MutableLiveData(false)
-    var showUpdateBtn : MutableLiveData<Boolean> = MutableLiveData(false)
-    var showDeleteBtn : MutableLiveData<Boolean> = MutableLiveData(false)
-    val context = getApplication<Application>().applicationContext
+    var contents = MutableLiveData("")
+    var statetype = MutableLiveData(StateType.TodayInit)
 
-    fun dateChange(year : Int, month : Int, dayOfMonth : Int){
-        showDiaryTextView.value = true
-        showSaveBtn.value = true
-        showContextEditText.value = true
-        showDiaryContent.value = false
-        showUpdateBtn.value = false
-        showDeleteBtn.value = false
-        diaryTextView.value = String.format("%d / %d / %d", year, month+1, dayOfMonth)
-        contextEditText.value = ""
-        checkDay(year, month, dayOfMonth, userID)
-    }
-
-    fun onSaveBtnClick(){
-        saveDiary(fname)
-        showContextEditText.value = false
-        showSaveBtn.value = false
-        showUpdateBtn.value = true
-        showDeleteBtn.value = true
-        str = contextEditText.value?:""
-        contents.value = str
-        showDiaryContent.value = true
-    }
-
-    fun onUpdateBtnClick(){
-        showContextEditText.value = true
-        showDiaryContent.value = false
-        contextEditText.value = str
-        showSaveBtn.value = true
-        showUpdateBtn.value = false
-        showDeleteBtn.value = false
-        contents.value = contextEditText.value?:""
-    }
-
-    fun onDeleteBtnClick(){
-        showDiaryContent.value = false
-        showUpdateBtn.value = false
-        showDeleteBtn.value = false
-        contextEditText.value = ""
-        showContextEditText.value = true
-        showSaveBtn.value = true
-        removeDiary(fname)
+    fun stateRefresh(stateType: StateType){
+        statetype.value = stateType
     }
 
     // 달력 내용 조회, 수정
-    fun checkDay(cYear: Int, cMonth: Int, cDay: Int, userId: String){
+    fun checkDay(cYear: Int, cMonth: Int, cDay: Int, fileDir: File) {
         //저장할 파일 이름설정
         fname = "" + userID + cYear + "-" + (cMonth+1) + "" + "-"+ cDay + ".txt"
-
-        var fileInputStream: FileInputStream
+        println("fname = $fname")
+        var str:String? = null
         try{
-            fileInputStream = context.openFileInput(fname)
-            val fileData = ByteArray(fileInputStream.available())
-            fileInputStream.read(fileData)
-            fileInputStream.close()
-            str = String(fileData)
-            showContextEditText.value = false
-            showDiaryContent.value = true
-            contents.value = str
-            showSaveBtn.value = false
-            showUpdateBtn.value = true
-            showDeleteBtn.value = true
+            File(fileDir, fname).inputStream().buffered().use{
+                val fileData = ByteArray(it.available())
+                it.read(fileData)
+                str = String(fileData)
+            }
         }catch (e:Exception){
             e.printStackTrace()
         }
+        stateRefresh(StateType.ShowSchedule)
+        if(str!=null) contents.value = str else contents.value = ""
     }
 
     // 달력 내용 제거
     @SuppressLint("WrongConstant")
-    fun removeDiary(readDay: String?){
-        val fileOutputStream: FileOutputStream
+    fun removeDiary(fileDir : File){
         try {
-            fileOutputStream = context.openFileOutput(readDay,
-                AppCompatActivity.MODE_NO_LOCALIZED_COLLATORS
-            )
-            val content = ""
-            fileOutputStream.write(content.toByteArray())
-            fileOutputStream.close()
+//            )
+            File(fileDir, fname).outputStream().buffered().use{
+                it.write("".toByteArray())
+            }
         } catch (e:Exception){
             e.printStackTrace()
         }
@@ -108,15 +53,11 @@ class CalendarViewModel(application: Application) : AndroidViewModel(application
 
     // 달력 내용 추가
     @SuppressLint("WrongConstant")
-    fun saveDiary(readDay: String?){
-        var fileOutputStream: FileOutputStream
+    fun saveDiary(editTextString : String, fileDir : File){
         try {
-            fileOutputStream = context.openFileOutput(readDay,
-                AppCompatActivity.MODE_NO_LOCALIZED_COLLATORS
-            )
-            val content = contextEditText.value?:""
-            fileOutputStream.write(content.toByteArray())
-            fileOutputStream.close()
+            File(fileDir, fname).outputStream().buffered().use{
+                it.write(editTextString.toByteArray())
+            }
         }catch (e:Exception){
             e.printStackTrace()
         }
